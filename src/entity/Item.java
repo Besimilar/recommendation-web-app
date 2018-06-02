23a164a1e4e20f59ce119902b49c6c68dcd6827c
Similar <besimilar@gmail.com>
2018-06-02 11:25:44 -0400

    Modify Item Class to support Hash Function
    1. Override hashCode() and equals() to support Set operations in getFavoriteItems() in MySQLConnection Class

--- src/entity/Item.java
+++ src/entity/Item.java
@@ -16,8 +16,7 @@ public class Item {
   private double distance;
 
   /**
-   * This is a builder pattern in Java.
-   * Easy for storing into database.
+   * This is a builder pattern in Java. Easy for storing into database.
    */
   private Item(ItemBuilder builder) {
     this.itemId = builder.itemId;
@@ -79,6 +78,37 @@ public class Item {
     return obj;
   }
 
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((itemId == null) ? 0 : itemId.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    Item other = (Item) obj;
+    if (itemId == null) {
+      if (other.itemId != null) {
+        return false;
+      }
+    } else if (!itemId.equals(other.itemId)) {
+      return false;
+    }
+    return true;
+  }
+
+
   public static class ItemBuilder {
     private String itemId;
     private String name;
@@ -132,7 +162,7 @@ public class Item {
     public Item build() {
       return new Item(this);
     }
-    
+
     // test code
     public static void main() {
       Item item = new ItemBuilder().setAddress("address").setDistance(10).build();
